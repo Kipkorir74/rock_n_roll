0edb72df5b5dc0cfe87c8c87d5e9603f12432fc9
Kipkorir74 <49982743+Kipkorir74@users.noreply.github.com>
2022-05-12 13:14:03 +0300

    404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import BlogDetails from "./BlogDetails";
+import Unavailable from "./Unavailable";
 
 
 function App() {
@@ -20,6 +21,9 @@ function App() {
          </Route>
          <Route path= "/blogs/:id">
             <BlogDetails/>
+         </Route>
+         <Route path="*">
+           <Unavailable/>
          </Route>
        </Switch>
       </div>
---
+++ src/Unavailable.js
@@ -0,0 +1,12 @@
+const Unavailable = () => {
+    return ( 
+        <div className="not-found">
+            <h2>Sorry</h2> 
+            <p>the page you are looking for is unavailable</p>
+            <a href="/">Back to main page...</a>
+        </div>
+        
+     );
+}
+ 
+export default Unavailable;
